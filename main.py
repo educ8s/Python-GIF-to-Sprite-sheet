723a281e56a1e464cb8f9bac56520503d2a2ba58
Bart Nijssen <73212177+connect-cloud@users.noreply.github.com>
2022-11-20 11:56:04 +0100

    convert all gif files in script folder
    Convert all *.gif files in the folder the script is in by getting the dirlist

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
+from os import listdir
 from PIL import Image
 
 OUTPUT_SIZE = (64,64) # The output size of each frame (or tile or Sprite) of the animation
-FILENAME = "clock.gif" # The file to convert
 MONOCHROME = False # Do you want the output file to be b/w?
 
-gif = Image.open(FILENAME)
-print(f"Size: {gif.size}")
-print(f"Frames: {gif.n_frames}")
+for file in listdir():
+    if file.endswith('.gif'):
+        gif = Image.open(file)
+        print(f"Size: {gif.size}")
+        print(f"Frames: {gif.n_frames}")
 
-if MONOCHROME:
-	output = Image.new("1", (OUTPUT_SIZE[0] * gif.n_frames, OUTPUT_SIZE[1]), 0)
-else:
-	output = Image.new("RGB", (OUTPUT_SIZE[0] * gif.n_frames, OUTPUT_SIZE[1]))
+        if MONOCHROME:
+            output = Image.new("1", (OUTPUT_SIZE[0] * gif.n_frames, OUTPUT_SIZE[1]), 0)
+        else:
+            output = Image.new("RGB", (OUTPUT_SIZE[0] * gif.n_frames, OUTPUT_SIZE[1]))
 
-output_filename = f"icon_{gif.n_frames}_frames.bmp"
+        output_filename = f"icon_{gif.n_frames}_frames.bmp"
 
-for frame in range(0,gif.n_frames):
-	gif.seek(frame)
-	extracted_frame = gif.resize(OUTPUT_SIZE)
-	position = (OUTPUT_SIZE[0]*frame, 0)
-	output.paste(extracted_frame, position)
+        for frame in range(0,gif.n_frames):
+            gif.seek(frame)
+            extracted_frame = gif.resize(OUTPUT_SIZE)
+            position = (OUTPUT_SIZE[0]*frame, 0)
+            output.paste(extracted_frame, position)
 
-if not MONOCHROME:
-	output = output.convert("P", colors = 8)
-output.save(output_filename)
+        if not MONOCHROME:
+            output = output.convert("P", colors = 8)
+        output.save(output_filename)
